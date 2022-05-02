5320c921ff9d2012340ec09cd2f277cdac4f5673
Александр Кучеренко <g28xyz@ya.ru>
2022-05-02 16:06:51 +0000

    feat: запущен сервер

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   autoIndex: true,
