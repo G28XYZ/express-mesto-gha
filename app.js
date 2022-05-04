b06b66ea7b0102caf5916d0a1699548b5f959d0b
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-05-04 20:21:55 +0700

    feat: CORS all

--- app.js
+++ app.js
@@ -27,14 +27,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS
 app.use((req, res, next) => {
-  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  // if (allowedCors.includes(origin)) {
+  // }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
